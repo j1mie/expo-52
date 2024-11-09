cb7b0cf7c80d3bef4773b84dceb9b90b96599032
Jimmy Loughran <james.loughran@gmail.com>
2024-11-09 14:21:16 +0100

    Bare example

--- app/index.tsx
+++ app/index.tsx
@@ -1,15 +1,18 @@
-import { Text, View } from "react-native";
+import { css, html } from 'react-strict-dom';
+
+const styles = css.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+})
+
 
 export default function Index() {
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-    </View>
+    <html.main style={styles.container}>
+      <html.h1>Edit app/index.tsx to edit this screen.</html.h1>
+    </html.main>
   );
 }
